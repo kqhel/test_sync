f875de32892e4c8d0e695213e0d2cc047637a4c7
kqhel <ank303442@gmail.com>
2024-06-23 06:13:34 -0600

    Change tab for spaces

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 mod fs_server;
 
-use fs_server::FsServer;
 use clap::{arg, command, Command};
+use fs_server::FsServer;
 
 #[tokio::main]
 async fn main() {
     let command_result = command!()
-		.subcommand(
-			Command::new("serve")
-				.about("Starts filesystem server.")
-				.arg(
-					arg!(-v --verbose "Enables debug/verbose mode.")
-				)
-		)
-		.get_matches();
+        .subcommand(
+            Command::new("serve")
+                .about("Starts filesystem server.")
+                .arg(arg!(-v --verbose "Enables debug/verbose mode.")),
+        )
+        .get_matches();
 
-	match command_result.subcommand() {
-		Some(("serve", _argument_matches)) => {
-			let mut fs_server = FsServer::new();
+    match command_result.subcommand() {
+        Some(("serve", _argument_matches)) => {
+            let mut fs_server = FsServer::new();
 
-			let watch_handle = fs_server.watch().await;
-			let server_handle = fs_server.start().await;
-			
-			let (_, _) = tokio::join!(watch_handle, server_handle);
-		},
+            let watch_handle = fs_server.watch().await;
+            let server_handle = fs_server.start().await;
 
-		_ => ()
-	}
+            let (_, _) = tokio::join!(watch_handle, server_handle);
+        }
+
+        _ => (),
+    }
 }
